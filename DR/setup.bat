359f09a2698db07f5158f7e80c27aa995584f725
fallingmeteorite <2327667836@qq.com>
2023-02-27 13:10:35 +0800

    fix_bug

--- DR/setup.bat
+++ DR/setup.bat
@@ -2,7 +2,7 @@
 CALL activate %~dp0venv
 pip list
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116 --no-warn-script-location
-pip install --use-pep517 --upgrade -r requirements\requirements.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install --use-pep517 --upgrade -r RE\requirements.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install xformers-0.0.16-cp310-cp310-win_amd64.whl
 python tools\cudann_1.8_install.py
 copy .\bitsandbytes_windows\*.dll .\venv\Lib\site-packages\bitsandbytes\
