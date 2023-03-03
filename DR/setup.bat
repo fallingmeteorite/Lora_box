8f4aa99b64f551ae023947cd68d2c1baada0ab6e
fallingmeteorite <2327667836@qq.com>
2023-03-03 22:43:56 +0800

    update

--- DR/setup.bat
+++ DR/setup.bat
@@ -7,7 +7,7 @@ python setup.py install
 cd..
 cd lion-pytorch-main
 python setup.py install
-cd..
+
 pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install xformers-0.0.16-cp310-cp310-win_amd64.whl
 python tools\cudann_1.8_install.py
