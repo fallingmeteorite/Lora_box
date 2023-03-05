9a25f66ee7029a6973c164f6ff3f42ffee7cbbf4
fallingmeteorite <2327667836@qq.com>
2023-03-05 10:37:16 +0800

    update

--- DR/setup.bat
+++ DR/setup.bat
@@ -8,6 +8,7 @@ python setup.py install
 cd..
 cd lion-pytorch-main
 python setup.py install
+cd..
 pip install xformers-0.0.17+b89a493.d20230304-cp310-cp310-win_amd64.whl
 pip install dynamo
 pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
