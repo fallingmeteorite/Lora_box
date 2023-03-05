a63ceb088837f2cf7ba24bf14768836e30fa707e
fallingmeteorite <2327667836@qq.com>
2023-03-05 14:30:58 +0800

    fix_bug

--- DR/fasttaggter.bat
+++ DR/fasttaggter.bat
@@ -3,5 +3,8 @@ cd /d %curpath%
 set exename=fastTagger.zip
 set downurl=https://huggingface.co/Fallingmeteorite/xformers-0.0.17/resolve/main/fastTagger.zip
 powershell curl -o "%exename%" "%downurl%"
+CALL activate %~dp0venv
 python install_fasttagger.py
+cd fastTagger\stable-diffusion-webui
+python launch.py --ui-debug-mode
 pause
