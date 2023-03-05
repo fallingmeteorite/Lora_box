0527dd362daadee5cdad32f313156029440909fc
fallingmeteorite <2327667836@qq.com>
2023-03-05 10:52:45 +0800

    update

--- DR/setup.bat
+++ DR/setup.bat
@@ -11,7 +11,7 @@ python setup.py install
 cd..
 pip install xformers-0.0.17+b89a493.d20230304-cp310-cp310-win_amd64.whl
 pip install dynamo
-pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install --use-pep517 --upgrade -r DE\RE.txt --no-warn-script-location -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install basicsr==1.4.2
 python tools\cudann_1.8_install.py
 copy .\bitsandbytes_windows\*.dll .\venv\Lib\site-packages\bitsandbytes\
