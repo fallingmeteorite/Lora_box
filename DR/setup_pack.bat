6524af8003da4ec91b02b903c62bbb356338efc8
fallingmeteorite <2327667836@qq.com>
2023-03-26 12:05:17 +0800

    update

--- DR/setup_pack.bat
+++ DR/setup_pack.bat
@@ -8,4 +8,5 @@ python setup_pack.py
 copy .\bitsandbytes_windows\*.dll .\venv\Lib\site-packages\bitsandbytes\
 copy .\bitsandbytes_windows\cextension.py .\venv\Lib\site-packages\bitsandbytes\cextension.py
 copy .\bitsandbytes_windows\main.py .\venv\Lib\site-packages\bitsandbytes\cuda_setup\main.py
+copy distributed_c10d.py .\venv\Lib\site-packages\torch\distributed
 pause
--- DR/setup_pack.py
+++ DR/setup_pack.py
@@ -42,6 +42,7 @@ try:
     os.system(r'pip install torch-2.0.0.dev20230202+cu116-cp310-cp310-win_amd64.whl')
     os.system(r'pip install torchvision-0.15.0.dev20230206+cu116-cp310-cp310-win_amd64.whl')
     os.system(r'pip install torchaudio-2.0.0.dev20230206+cu116-cp310-cp310-win_amd64.whl')
+    os.system(r'pip install dynamo==0.1.1')
     print(r"==========================================================================================================")
     print(r"torch安装成功")
 except:
@@ -50,7 +51,7 @@ except:
 try:
     os.system(r'pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i '+net)
     os.system(r'pip install xformers-0.0.17+b89a493.d20230304-cp310-cp310-win_amd64.whl')
-    os.system(r'pip install deepspeed-0.8.1+unknown-py3-none-any.whl')
+    os.system(r'pip install triton-2.0.0-cp310-cp310-win_amd64.whl')
     print(r"==========================================================================================================")
     print(r"依赖安装成功")
 except:
